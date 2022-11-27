dfe0fa3794fc665af4f776b26a6a0ab78c7f75fb
Yoga Nur Alam <107308982+yogArt12@users.noreply.github.com>
2022-11-28 02:21:22 +0700

    Add files via upload

---
+++ beranda.php
@@ -0,0 +1,28 @@
+<div class="content-wrapper" style="min-height: 416px;">
+<!-- Content Header (Page header) -->
+<div class="content-header">
+<div class="container-fluid">
+<div class="row mb-2">
+<div class="col-sm-6">
+<h1 class="m-0">Beranda <small>Halaman Admin</small></h1>
+</div><!-- /.col -->
+<div class="col-sm-6">
+<ol class="breadcrumb float-sm-right">
+<li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i> Home</a></li>
+<li class="breadcrumb-item active">Dashboard v1</li>
+</ol>
+</div><!-- /.col -->
+</div><!-- /.row -->
+</div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<!-- Main content -->
+<section class="content">
+
+<div class="container-fluid">
+<h5> Silahkan Pilih Menu Disamping Untuk Mengolah Data.</h5>
+</div><!-- /.container-fluid -->
+
+</section>
+<!-- /.content -->
+</div>
